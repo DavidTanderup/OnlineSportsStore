d49d9d8cb23b0bab3acc868c1fa08e0ac0f1cd80
David Tanderup <Davetanderup@gmail.com>
2019-05-23 15:41:45 -0400

    Adds new URL Scheme in the RouteConfig

--- SportsStore.WebUI/App_Start/RouteConfig.cs
+++ SportsStore.WebUI/App_Start/RouteConfig.cs
@@ -7,28 +7,43 @@ using System.Web.Routing;
 
 namespace SportsStore.WebUI
 {
+
     public class RouteConfig
     {
+
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: null,
-                url: "Page{page}",
-                defaults: new { Controller = "Product", action = "List" }
-            );
-
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new
+            routes.MapRoute(null,
+                "",
+                new
                 {
                     controller = "Product",
                     action = "List",
-                    id = UrlParameter.Optional
+                    category = (string)null,
+                    page = 1
                 }
             );
+
+            routes.MapRoute(null,
+                "Page{page}",
+                new { controller = "Product", action = "List", category = (string)null },
+                new { page = @"\d+" }
+            );
+
+            routes.MapRoute(null,
+                "{category}",
+                new { controller = "Product", action = "List", page = 1 }
+            );
+
+            routes.MapRoute(null,
+                "{category}/Page{page}",
+                new { controller = "Product", action = "List" },
+                new { page = @"\d+" }
+            );
+
+            routes.MapRoute(null, "{controller}/{action}");
         }
     }
 }
