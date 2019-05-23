9ca335d6856a668c25f9ceebc1e899a3195b8453
David Tanderup <Davetanderup@gmail.com>
2019-05-23 10:36:38 -0400

    Makes the class do stuff

--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -1,11 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Web.Mvc;
+using Ninject;
 
 namespace SportsStore.WebUI.Infrastructure
 {
-    public class NinjectDependencyResolver
+
+    public class NinjectDependencyResolver : IDependencyResolver
     {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            // put bindings here
+        }
     }
 }
