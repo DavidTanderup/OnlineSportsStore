03f113cef49cf2ac8dec7c37c2fccebd3505cb53
David Tanderup <Davetanderup@gmail.com>
2019-05-23 10:47:29 -0400

    Adds Mock IProductRepository Implementation in the NinjectDependencyResolver

--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
+using Moq;
 using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -28,7 +32,14 @@ namespace SportsStore.WebUI.Infrastructure
 
         private void AddBindings()
         {
-            // put bindings here
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product> {
+                new Product { Name = "Football", Price = 25 },
+                new Product { Name = "Surf board", Price = 179 },
+                new Product { Name = "Running shoes", Price = 95 }
+            });
+
+            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
         }
     }
 }
