c2ba6f01b9e7c50bc50235b212142bd3ac1113d8
David Tanderup <Davetanderup@gmail.com>
2019-05-25 17:01:00 -0400

    Adds Admin Controller

---
+++ SportsStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+
+namespace SportsStore.WebUI.Controllers
+{
+
+    public class AdminController : Controller
+    {
+        private IProductRepository repository;
+
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index()
+        {
+            return View(repository.Products);
+        }
+    }
+}
