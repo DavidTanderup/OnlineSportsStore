740d7724ca92467a8e13f127f8677389109605f2
David Tanderup <Davetanderup@gmail.com>
2019-05-25 16:36:18 -0400

    Updating the Menu Action Method in the NavController

--- SportsStore.WebUI/Controllers/NavigationController.cs
+++ SportsStore.WebUI/Controllers/NavigationController.cs
@@ -1,30 +1,33 @@
-﻿using SportsStore.Domain.Abstract;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using System.Linq;
 
 namespace SportsStore.WebUI.Controllers
 {
-    public class NavigationController : Controller
+
+    public class NavController : Controller
     {
         private IProductRepository repository;
 
-        public NavigationController(IProductRepository repo)
+        public NavController(IProductRepository repo)
         {
             repository = repo;
         }
 
-        public PartialViewResult Menu(string category = null)
+        public PartialViewResult Menu(string category = null,
+            bool horizontalLayout = false)
         {
+
             ViewBag.SelectedCategory = category;
+
             IEnumerable<string> categories = repository.Products
                                     .Select(x => x.Category)
                                     .Distinct()
                                     .OrderBy(x => x);
 
-            return PartialView(categories);
+            string viewName = horizontalLayout ? "MenuHorizontal" : "Menu";
+            return PartialView(viewName, categories);
         }
     }
 }
