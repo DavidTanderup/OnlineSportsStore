f086ad0d4d3916722bdbd1d638766d403a4faea4
David Tanderup <Davetanderup@gmail.com>
2019-05-25 12:33:18 -0400

    Add Checkout to Cartcontroller

--- SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -55,5 +55,9 @@ namespace SportsStore.WebUI.Controllers
         {
             return PartialView(cart);
         }
+        public ViewResult Checkout()
+        {
+            return View(new ShippingDetails());
+        }
     }
 }
