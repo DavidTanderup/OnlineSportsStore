9dda8181ed653f69e638eb801ec67ead428d3ca2
David Tanderup <Davetanderup@gmail.com>
2019-05-25 11:11:58 -0400

    Implementing the Menu Method

---
+++ SportsStore.WebUI/Controllers/NavigationController.cs
@@ -0,0 +1,29 @@
+﻿using SportsStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class NavigationController : Controller
+    {
+        private IProductRepository repository;
+
+        public NavigationController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public PartialViewResult Menu()
+        {
+            IEnumerable<string> categories = repository.Products
+                                    .Select(x => x.Category)
+                                    .Distinct()
+                                    .OrderBy(x => x);
+
+            return PartialView(categories);
+        }
+    }
+}
